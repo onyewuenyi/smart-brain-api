27f1629246e6dfd7df7dd09ce2034e0d0d007ad4
onyewuenyi <onyewuenyi@gmail.com>
2018-05-15 18:33:22 -0500

    modified sever host

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-dimensional-45093',
     user : 'charlesonyewuenyi',
     password : '',
     database : 'smart-brain'
